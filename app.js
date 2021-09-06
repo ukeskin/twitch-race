3f79684b522b23f74d3127975c90b33059e7e723
Umut Keskin <ukeskin2626@gmail.com>
2021-09-06 13:05:54 +0300

    Add two car

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const box = document.getElementById('box');
-
+const car_red = document.getElementById('car_red');
+const car_blue = document.getElementById('car_blue');
 const client = new tmi.Client({
 	channels: [ 'umutkeskin26' ]
 });
 
 client.connect();
 
-let count = 0;
-
+let count_red = 0;
+let count_blue = 0;
+        
 client.on('message', (channel, tags, message, self) => {
     if (self) return true;
-    if (message === '!box') {
-        count+= 10;
-        box.style.left = count + 'px';
+    if (message === '!red') {
+        count_red += 10;
+        car_red.style.left = count_red + 'px';
+    } 
+    if (message === '!blue') {
+        count_blue += 10;
+        car_blue.style.left = count_blue + 'px';
     }
 });
-
